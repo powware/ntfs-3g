fb7a1640dfa4e071a73961d26992d0adab0e2b07
Pete Batard <pete@akeo.ie>
2021-07-21 17:26:19 +0100

    Fix MSVC Intx constant handling
    MSVC does not support 64-bit enums, so all the enum definitions resolved
    to the same 32-bit "Intx" value. Fix this by using #define 64-bit constants.

--- include/ntfs-3g/layout.h
+++ include/ntfs-3g/layout.h
@@ -2712,17 +2712,13 @@ typedef struct {
 
 typedef EFS_DF_CERTIFICATE_THUMBPRINT_HEADER EFS_DF_CERT_THUMBPRINT_HEADER;
 
-typedef enum {
-	INTX_SYMBOLIC_LINK =
-		const_cpu_to_le64(0x014B4E4C78746E49ULL), /* "IntxLNK\1" */
-	INTX_CHARACTER_DEVICE =
-		const_cpu_to_le64(0x0052484378746E49ULL), /* "IntxCHR\0" */
-	INTX_BLOCK_DEVICE =
-		const_cpu_to_le64(0x004B4C4278746E49ULL), /* "IntxBLK\0" */
-} INTX_FILE_TYPES;
+/* MSVC does not support 64-bit enum */
+#define INTX_SYMBOLIC_LINK    const_cpu_to_le64(0x014B4E4C78746E49ULL) /* "IntxLNK\1" */
+#define INTX_CHARACTER_DEVICE const_cpu_to_le64(0x0052484378746E49ULL) /* "IntxCHR\0" */
+#define INTX_BLOCK_DEVICE     const_cpu_to_le64(0x004B4C4278746E49ULL) /* "IntxBLK\0" */
 
 typedef struct {
-	INTX_FILE_TYPES magic;		/* Intx file magic. */
+	le64 magic;				/* Intx file magic. */
 	union {
 		/* For character and block devices. */
 		struct {
